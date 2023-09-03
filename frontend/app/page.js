432c322e80f96ea3dfa4fce7225cfd1c20e9e2b9
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-09-03 21:30:34 +0200

    Seriously?

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.scss";
 // Edit this out later
 
 const getCategories = async () => {
-	const data = await fetch(`http://localhost:1337/categories`);
+	const data = await fetch(`http://localhost:1337/api/categories`);
 	const categories = await data.json();
 	console.log(categories);
 };
