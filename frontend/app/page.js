55183ed98e94d736e9d224004ff47aeb9fc5bc81
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-09-08 21:28:13 +0200

    the struggles

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -9,12 +9,15 @@ import Categories from "./components/Categories";
 // Edit this out later
 
 export default function Home() {
-	const { categories } = useContext(CardContext);
+	const { data } = useContext(CardContext);
+
+	console.log(data?.categories);
 
 	return (
 		<main className={styles.main}>
-			{categories &&
-				categories.map((category) => (
+			{data &&
+				data.categories.data &&
+				data.categories.data.map((category) => (
 					<Categories key={category.id} category={category} /> // Note that I'm passing the category object as a prop here
 				))}
 		</main>
